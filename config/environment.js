ef7fcc48eb75c798f44b24e5bf9e166eccee6f87
Shawn Gibson <saljgib@gmail.com>
2015-07-22 11:13:07 -0400

    changed config file to update stripe publishable key

--- config/environment.js
+++ config/environment.js
@@ -54,9 +54,7 @@ module.exports = function(environment) {
     // Testem prefers this...
     ENV.baseURL = '/';
     ENV.locationType = 'none';
-    ENV.Stripe = {
-      publishableKey: 'pk_test_rZElzrV3eXXSjJM5fqXVxUzF'
-    };
+    ENV.Stripe.publishableKey: 'pk_test_rZElzrV3eXXSjJM5fqXVxUzF';
 
 
     // keep test console output quieter
